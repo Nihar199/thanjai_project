66f906268635df5f18e76d2ee4e009a0315f81a7
Nihar199 <95146839+Nihar199@users.noreply.github.com>
2024-10-12 10:39:22 -0400

    scratch

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import axios from 'axios';
+import React from 'react';
+class App extends React.Component {
+  state = {details:[],}
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/')
+      .then(res => {
+        this.setState({ details: res.data });
+      })
+      .catch(err => {
+        console.error("Error fetching data:", err);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <header> Data Generated </header>
+        <hr></hr>
+        {this.state.details.map((output, id)=> (
+          <div key ={id}> 
+          <div>
+            <h2>{output.employee}</h2>
+            <h3>{output.department}</h3>
+
+          </div>
+        </div>))}
+
+      </div>
+
+
+
+    )
+  }
+
+}
+
+
+export default App;
